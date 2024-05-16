d4dce9ee388ce87c33a0cdc55f8772bca8e9e7a4
hongsy521 <seoyeong0521@naver.com>
2024-05-16 13:09:37 +0900

    initial commit

---
+++ src/test/java/com/sparta/schedulemanagement_spring/ScheduleManagementSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulemanagement_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
